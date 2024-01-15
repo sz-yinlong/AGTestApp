6f7dd796251331f6e8b546a90d2e3b6233876641
Artem Bariev <szyinlong@gmail.com>
2024-01-15 12:57:43 +0300

    init

---
+++ AGTestApp/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  AGTestApp
+//
+//  Created by Artem on 15.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
---
+++ AGTestApp/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  AGTestApp
+//
+//  Created by Artem on 15.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
